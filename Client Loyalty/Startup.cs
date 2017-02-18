fcf7c6237c4602b177593637fd57977d2526342d
Rafael Antonio Morales <rafael.7.morales@gmail.com>
2017-02-18 14:33:55 -0600

    Agregar archivos de proyecto.

---
+++ Client Loyalty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Client_Loyalty.Startup))]
+namespace Client_Loyalty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
