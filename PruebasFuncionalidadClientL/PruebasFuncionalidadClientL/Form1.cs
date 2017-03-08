d1018b67d277819344eddd3c55ebff774d3d817f
Rafael Antonio Morales <rafael.7.morales@gmail.com>
2017-03-08 17:18:43 -0600

    listo el Log In esperando cadenas a recibir

--- PruebasFuncionalidadClientL/PruebasFuncionalidadClientL/Form1.cs
+++ PruebasFuncionalidadClientL/PruebasFuncionalidadClientL/Form1.cs
@@ -1,33 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.ComponentModel;
 using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Data.Sql;
 using System.Data.SqlClient;
 
-namespace Client_Loyalty
+namespace PruebasFuncionalidadClientL
 {
-    public partial class LogIn : System.Web.UI.Page
+    public partial class Form1 : Form
     {
         SqlConnection cn;
         SqlCommand cmd;
         SqlDataReader dr;
 
-        protected void Page_Load(object sender, EventArgs e)
+        public Form1()
         {
-            String user="";//RECIBIRA EL USUARIO INGRESADO
-            String pass=""; //RECIBIRA LA CONTRASEÑA
-            if(verificarLogIn(user,pass) == true)
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String user = "";//RECIBIRA EL USUARIO INGRESADO
+            String pass = ""; //RECIBIRA LA CONTRASEÑA
+            user = this.textBox1.Text;//ramd777
+            pass = textBox2.Text;     //7189rafa
+
+            if (verificarLogIn(user, pass) == true)
             {
                 //DIRIGIRSE A LA SIGUIENTE PAGINA
-            }else
-            {
-                Response.Write("<script>window.alert('Usuario o Contraseña Incorrecta');</script>");
+                MessageBox.Show("¡CORRECTO!");
             }
-
+            else
+            {
+                //Response.Write("<script>window.alert('Usuario o Contraseña Incorrecta');</script>");
+                MessageBox.Show("USUARIO O CONTRASEÑA INCORRECTO!");
+            }
         }
 
         public bool verificarLogIn(String usu, String pass)
@@ -47,12 +59,12 @@ namespace Client_Loyalty
                 }
 
                 cn.Close();
-                if (bandera != 0)
+                if(bandera != 0)
                 {
                     return true;
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return false;
             }
