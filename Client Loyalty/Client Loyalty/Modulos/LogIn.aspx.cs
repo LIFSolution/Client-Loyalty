a5de42f303d3e2d055905e0800509d6f730178c9
Rafael Antonio Morales <rafael.7.morales@gmail.com>
2017-03-08 16:18:26 -0600

    continuacion de coneccion con el log in

--- Client Loyalty/Client Loyalty/Modulos/LogIn.aspx.cs
+++ Client Loyalty/Client Loyalty/Modulos/LogIn.aspx.cs
@@ -18,7 +18,15 @@ namespace Client_Loyalty
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            //CADENA DE CONECCION     Data Source=LAPTOP-F8V8Q28T\MYHPSQL;Initial Catalog=LifsControlDb;Integrated Security=True
+            String user="";//RECIBIRA EL USUARIO INGRESADO
+            String pass=""; //RECIBIRA LA CONTRASEÑA
+            if(verificarLogIn(user,pass) == true)
+            {
+                //DIRIGIRSE A LA SIGUIENTE PAGINA
+            }else
+            {
+                Response.Write("<script>window.alert('Usuario o Contraseña Incorrecta');</script>");
+            }
 
         }
 
@@ -28,6 +36,13 @@ namespace Client_Loyalty
             {
                 cn = new SqlConnection("Data Source=LAPTOP-F8V8Q28T\\MYHPSQL;Initial Catalog=LifsControlDb;Integrated Security=True");
                 cn.Open();
+                cmd = new SqlCommand(string.Format("Select IDLogIn, Contrasena from Usuarios where IDLogIn like '%{0}%' and Contrasena like '%{1}%'", user, pass),cn);
+
+                dr = cmd.ExecuteReader();
+                if((user.Equals(dr.GetString(0))) && pass.Equals(dr.GetString(1)))
+                {
+                    return true;
+                }
             }
             catch(Exception ex)
             {
