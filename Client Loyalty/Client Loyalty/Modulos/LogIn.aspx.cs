2c35baf5b85a6b5f1ef5924cb746013f09ffe3d8
Rafael Antonio Morales <rafael.7.morales@gmail.com>
2017-03-08 15:34:04 -0600

    inicio de coneccion con BD

---
+++ Client Loyalty/Client Loyalty/Modulos/LogIn.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Client_Loyalty
+{
+    public partial class LogIn : System.Web.UI.Page
+    {
+        SqlConnection cn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //CADENA DE CONECCION     Data Source=LAPTOP-F8V8Q28T\MYHPSQL;Initial Catalog=LifsControlDb;Integrated Security=True
+
+        }
+
+        public bool verificarLogIn(String user, String pass)
+        {
+            try
+            {
+                cn = new SqlConnection("Data Source=LAPTOP-F8V8Q28T\\MYHPSQL;Initial Catalog=LifsControlDb;Integrated Security=True");
+                cn.Open();
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+            return false;
+        }
+    }
+}
