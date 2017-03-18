6f69745db48900b39f5daa0a1f2d2e6fa84771fb
Rafael Antonio Morales <rafael.7.morales@gmail.com>
2017-03-18 14:00:43 -0600

    arreglo base datos , orm
    agregar entity framework y carpetar a trabajr

--- Client Loyalty/Client Loyalty/Controllers/AccountController.cs
+++ Client Loyalty/Client Loyalty/Controllers/AccountController.cs
@@ -72,7 +72,7 @@ namespace Client_Loyalty.Controllers
             {
                 return View(model);
             }
-
+            
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
---
+++ Client Loyalty/Client Loyalty/Models/LifsLogic/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Client_Loyalty.Models.LifsLogic
+{
+    public class LoginLogic
+    {
+        public int IngresoNuevoUsuario(lifsEF.Usuarios Miusuario)
+        {
+            int iresultado = 0;
+            //evaluar usuario 
+            lifsEF.LifsControlDbEntities dbcontext = new lifsEF.LifsControlDbEntities();
+            dbcontext.Usuarios.Add(Miusuario);
+            
+            return iresultado;
+        }
+    }
+}
